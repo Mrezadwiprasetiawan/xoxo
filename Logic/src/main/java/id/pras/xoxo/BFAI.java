f2706ed892b83983c5e0f64b3443cf5d1583402f
M Reza Dwi Prasetiawan <95429659+Mrezadwiprasetiawan@users.noreply.github.com>
2024-09-21 16:34:14 +0700

    some update abstract BFAI.java

--- Logic/src/main/java/id/pras/xoxo/BFAI.java
+++ Logic/src/main/java/id/pras/xoxo/BFAI.java
@@ -1,50 +1,49 @@
 package id.pras.xoxo;
 
-public class BFAI extends AI{
+public class BFAI extends AI {
   private String dataPath;
   private int ThreadCount;
-  private boolean hasData=false;
-  public BFAI(int[][] board){
-		this(board,3,3);
-		setThreadCount(1);
-	}
-	
-	public BFAI(int[][] board, int sideSize, int winSize){
-		super(board,sideSize,winSize);
-	}
-	
-	public void setThreadCount(int ThreadCount){
-		this.ThreadCount=ThreadCount;
-	}
-	
-	public void train(){
-		setRunning(true);
-		int div=sideSize()/ThreadCount;
-		int rem=sideSize()%ThreadCount;
-		int first=div+rem;
-		
-		for(int i=0;i<ThreadCount;i++){
-			if(i==1){
-				
-			}
-		}
-		
-		this.hasData=true;
-		setRunning(false);
-	}
-	
+  private boolean hasData = false;
+
+  public BFAI(int[][] board) {
+    this(board, 3, 3);
+    setThreadCount(1);
+  }
+
+  public BFAI(int[][] board, int sideSize, int winSize) {
+    super(board, sideSize, winSize);
+  }
+
+  public void setThreadCount(int ThreadCount) {
+    this.ThreadCount = ThreadCount;
+  }
+
+  public void train(int i) {
+    //implementasi kode pelatihan
+    try{
+      writeData(datas);
+      this.hasData=true;
+    }
+    catch(IOException){
+      this.hasData=false;
+    }
+  }
+
   @Override
   protected void Thinking() {
-  	for(int i=0; i<ThreadCount;i++){
-		  new Thread(()->{
-				if(hasData){
-					//implementasi pembacaan data disini
-				}else{
-					//implementasi kode bruteforcing disini
-				}
-			});
-		}
+    setRunning(true);
+    for (int i = 0; i < ThreadCount; i++) {
+      new Thread(() -> {
+        if (hasData) {
+          // implementasi pembacaan data di sini
+        } else {
+          // implementasi kode bruteforcing di sini
+        }
+      }).start(); // Pastikan thread dijalankan
+    }
+    setRunning(false);
   }
-  
-  
+  private void writeData(Data[] data){
+     
+  }
 }
