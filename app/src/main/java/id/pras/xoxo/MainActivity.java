af385d3a9b308d1cef87ed8c12332dfce70acd7e
M Reza Dwi Prasetiawan <95429659+Mrezadwiprasetiawan@users.noreply.github.com>
2024-09-21 15:19:58 +0700

    init

---
+++ app/src/main/java/id/pras/xoxo/MainActivity.java
@@ -0,0 +1,52 @@
+package id.pras.xoxo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import id.pras.xoxo.ui.Board;
+import id.pras.xoxo.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+
+  private ActivityMainBinding binding;
+  private Thread winState;
+  private boolean running = false;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+    Board board = new MultiPlayerBoard(getBaseContext(),25, 5);
+    board.setLayoutParams(
+        new ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+    binding.getRoot().addView(board);
+    winState =
+        new Thread(
+            () -> {
+              while (!board.getWinState()) {
+                this.running = true;
+              }
+              this.running = false;
+              runOnUiThread(
+                  () -> {
+                    binding.getRoot().removeView(board);
+                  });
+            });
+	  winState.start();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    this.binding = null;
+    if (this.winState != null && this.winState.isAlive()) {
+      winState.interrupt();
+    }
+    winState = null;
+  }
+  
+      }
+      
